e3cadf60a15effb09600973e22d02cfa05415c0d
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-07-21 20:23:56 +0700

    add custem element buttons,custom atribut and finish hero section

---
+++ components/buttons.js
@@ -0,0 +1,87 @@
+class ButtonLeft extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "text") {
+      this.render();
+    }
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .button {
+          background: linear-gradient(90deg, #e100ff, #00aaff);
+          color: #fff;
+          padding: 10px 20px;
+          border-radius: 100px;
+          border: none;
+          cursor: pointer;
+          text-align: center;
+          display: inline-block;
+        }
+      </style>
+      <button class="button">${this.getAttribute("text")}</button>
+    `;
+  }
+}
+
+customElements.define("button-left", ButtonLeft);
+
+class ButtonRight extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "text") {
+      this.render();
+    }
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .button {
+          background: transparent;
+          color: #fff;
+          padding: 10px 20px;
+          border: 1px solid #fff;
+          border-radius: 100px;
+          cursor: pointer;
+          text-align: center;
+          display: inline-block;
+          transition: all 0.5s ease;
+        }
+        .button:hover {
+          color: #fff;
+          border-color: transparent;
+          transform: scale(1.05);
+        }
+      </style>
+      <button class="button">${this.getAttribute("text")}</button>
+    `;
+  }
+}
+
+customElements.define("button-right", ButtonRight);
