e41c3ce1a831f7b4a8fba1a6cd169a4ce9bc6608
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-07-23 11:22:21 +0700

    add footer

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const scrollToNoteFormButton = document.querySelector("#scroll-to-note-form");
-
-  scrollToNoteFormButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    const noteFormSection = document.querySelector("#note-form");
-    noteFormSection.scrollIntoView({ behavior: "smooth" });
-  });
-});
